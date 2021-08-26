00cda734a979a7beca4a510acebb61da2279ae0b
brunofilipe.mirage@gmail.com <brunofilipe.mirage@gmail.com>
2021-08-26 15:54:18 -0300

    Projeto Criado

---
+++ src/main/java/silvafilipe/com/fullstackcourse_springboot_silvafilipe/FullstackcourseSpringbootSilvafilipeApplication.java
@@ -0,0 +1,13 @@
+package silvafilipe.com.fullstackcourse_springboot_silvafilipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackcourseSpringbootSilvafilipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackcourseSpringbootSilvafilipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/silvafilipe/com/fullstackcourse_springboot_silvafilipe/FullstackcourseSpringbootSilvafilipeApplicationTests.java
@@ -0,0 +1,13 @@
+package silvafilipe.com.fullstackcourse_springboot_silvafilipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackcourseSpringbootSilvafilipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
