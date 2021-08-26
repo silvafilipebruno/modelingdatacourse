06e528a197faff04b71f50be1c1b19a1c257defa
brunofilipe.mirage@gmail.com <brunofilipe.mirage@gmail.com>
2021-08-26 17:16:05 -0300

    Testando a primeira classe de domínio - Categoria

--- src/main/java/silvafilipe/com/fullstackcourse_springboot_silvafilipe/resources/CategoriaResource.java
+++ src/main/java/silvafilipe/com/fullstackcourse_springboot_silvafilipe/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package silvafilipe.com.fullstackcourse_springboot_silvafilipe.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import silvafilipe.com.fullstackcourse_springboot_silvafilipe.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categoria" )
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest ok";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2= new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();     
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
